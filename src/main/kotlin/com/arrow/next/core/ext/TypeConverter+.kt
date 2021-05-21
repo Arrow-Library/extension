1277c9c1f4987ec93284b984d06e73ada5e28339
Noon <er4goon@gmail.com>
2021-05-21 12:45:39 +0700

    Add Converter

--- src/main/kotlin/com/arrow/next/core/ext/TypeConverter+.kt
+++ src/main/kotlin/com/arrow/next/core/ext/TypeConverter+.kt
@@ -20,3 +20,23 @@ inline fun <reified T> toTypeOf(
         }
     return adapter.fromJson(json)!!
 }
+
+/**
+ * For some reason that API response key with UpperCase Moshi could not
+ * Convert so we have to format key to lowercase
+ */
+inline fun <reified T> toTypeOfForceMoshi(json: String): T? {
+    val reg = ",\"[A-Z]|\\{\"[A-Z]".toRegex()
+    val obj = reg.replace(json) { value ->
+        value.value.lowercase()
+    }
+    val moshi = Moshi.Builder().build()
+    val adapter = moshi.adapter<T>(T::class.java)
+    return adapter.fromJson(obj)
+}
+
+inline fun <reified T> toJson(obj: T): String {
+    val moshi = Moshi.Builder().build()
+    val adapter = moshi.adapter<T>(T::class.java)
+    return adapter.toJson(obj)
+}
